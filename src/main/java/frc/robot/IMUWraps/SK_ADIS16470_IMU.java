5130d59481269077526fa9071b17cdc0ac099f93
Shaun Kurian <shaunkurian345@gmail.com>
2022-10-20 15:28:22 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/IMUWraps/SK_ADIS16470_IMU.java
@@ -0,0 +1,173 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.IMUWraps;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * An IMU class that extends the ADIS16470_IMU and determines the acceleration
+ * in each axis and filters out the acceleration of gravity based on the
+ * calibration data
+ */
+public class SK_ADIS16470_IMU extends ADIS16470_IMU {
+
+    private SK_ADIS16470_IMU imu = new SK_ADIS16470_IMU();
+
+    /** The value of gravity */
+    private double gravity = 9.81;
+    /** The initial pitch calculated by the calibration data */
+    private double initPitch = 0;
+    private double deltaPitch = 0;
+    /** The initial roll calculated by the calibration data */
+    private double initRoll = 0;
+    private double deltaRoll = 0;
+    /** The initial yaw of the robot set by the calibration method */
+    private double initYaw = 0;
+    private double deltaYaw = 0;
+    /** The yaw that the imu is offset compared the robot front */
+    private double offsetYaw = 0;
+
+    /** Average acceleration in each axis {x, y, z} at startup */
+    private double[] calib = new double[3];
+    /** Number of data points needed for calibration */
+    private int calibPoints = 250;
+
+    /** Moving average data set */
+    private double[][] mvgAvg = new double[calibPoints][3];
+    /** The current index that will be updated for the mvgAvg array */
+    private int currentIndex = 0;
+
+    /**
+     * Creates a new IMU using the ADIS16470_IMU class.
+     * 
+     * @param yaw_axis The axis that measures the yaw
+     * @param port     The SPI Port the gyro is plugged into
+     * @param cal_time Calibration time
+     */
+    public SK_ADIS16470_IMU(IMUAxis yaw_axis, SPI.Port port, CalibrationTime cal_time) {
+        super(yaw_axis, port, cal_time);
+        axisCalibrate();
+    }
+
+    /** Creates a new IMU using the ADIS16470_IMU class. */
+    public SK_ADIS16470_IMU() {
+        super();
+        axisCalibrate();
+    }
+
+    /**
+     * Calibrate the gyro. It's important to make sure that the robot is not moving
+     * while the calibration is in progress, this is typically done when the robot
+     * is first turned on while it's sitting at rest before the match starts. This
+     * will find the average acceleration in each axis
+     * 
+     * @return An array containing the average acceleration in each axis {x, y, z}
+     *         at calibration time
+     */
+    private void axisCalibrate() {
+
+        // Resetting the calibration values
+        calib[0] = 0;
+        calib[1] = 0;
+        calib[2] = 0;
+
+        // Start collecting data to find average acceleration while standing still
+        for (int i = 0; i < calibPoints; i++) {
+
+            // Adding all the acceleration values
+            // on an axis by axis basis
+            mvgAvg[i][0] = imu.getAccelX();
+            mvgAvg[i][1] = imu.getAccelY();
+            mvgAvg[i][2] = imu.getAccelZ();
+
+            // Waiting 20 ms before collecting next set of data
+            Timer.delay(0.02);
+        }
+
+        averageData();
+        calculateInitAngles();
+        calculateGravity();
+    }
+
+    /**
+     * Reads the acceleration data from the IMU when called and adds it to the
+     * moving average data set. Only call once per schedule run.
+     */
+    public void addDataPoint() {
+        mvgAvg[currentIndex][0] = imu.getAccelX();
+        mvgAvg[currentIndex][1] = imu.getAccelY();
+        mvgAvg[currentIndex][2] = imu.getAccelZ();
+
+        currentIndex++;
+        currentIndex = (currentIndex >= calibPoints) ? 0 : currentIndex;
+    }
+
+    public void averageData() {
+        double sumX = 0;
+        double sumY = 0;
+        double sumZ = 0;
+
+        // Dividing the sums of the acceleration to find the average acceleration
+        // in each axis.
+        for (int i = 0; i < calibPoints; i++) {
+            sumX += mvgAvg[i][0];
+            sumY += mvgAvg[i][1];
+            sumZ += mvgAvg[i][3];
+        }
+
+        calib[0] = sumX / calibPoints;
+        calib[1] = sumY / calibPoints;
+        calib[2] = sumZ / calibPoints;
+    }
+
+    /**
+     * Calculate the pitch and roll of the IMU using the data from the calibration
+     */
+    private void calculateInitAngles() {
+        initPitch = -Math.atan(calib[1] / calib[2]);
+        initRoll = Math.atan(calib[0] / calib[2]);
+    }
+
+    /**
+     * Determines the measurable value of gravity. This should be done after the IMU
+     * has been calibrated while standing still
+     */
+    private void calculateGravity() {
+        // Calculating the magnitude of gravity
+        gravity = Math.sqrt(
+                Math.pow(calib[0], 2) +
+                        Math.pow(calib[1], 2) +
+                        Math.pow(calib[2], 2));
+    }
+
+    public double getRateX() {
+        return m_gyro_rate_x;
+    }
+
+    public double getRateY() {
+        return m_gyro_rate_y;
+    }
+
+    public double getRateZ() {
+        return m_gyro_rate_z;
+    }
+
+    public double getPitch()
+    {
+        return initPitch + deltaPitch;
+    }
+
+    public double getRoll()
+    {
+        return initRoll + deltaRoll;
+    }
+
+    public double getYaw()
+    {
+        return initYaw + deltaYaw;
+    }
+
+}
---
+++ src/main/java/frc/robot/tools/KalmanFilter.java
@@ -0,0 +1,238 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.tools;
+
+import edu.wpi.first.math.Drake;
+import edu.wpi.first.math.MathSharedStore;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.Num;
+import edu.wpi.first.math.StateSpaceUtil;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.system.Discretization;
+import edu.wpi.first.math.system.LinearSystem;
+
+
+/**
+ * A Kalman filter combines predictions from a model and measurements to give an estimate of the
+ * true system state. This is useful because many states cannot be measured directly as a result of
+ * sensor noise, or because the state is "hidden".
+ *
+ * <p>Kalman filters use a K gain matrix to determine whether to trust the model or measurements
+ * more. Kalman filter theory uses statistics to compute an optimal K gain which minimizes the sum
+ * of squares error in the state estimate. This K gain is used to correct the state estimate by some
+ * amount of the difference between the actual measurements and the measurements predicted by the
+ * model.
+ *
+ * <p>For more on the underlying math, read
+ * https://file.tavsys.net/control/controls-engineering-in-frc.pdf chapter 9 "Stochastic control
+ * theory".
+ */
+@SuppressWarnings("ClassTypeParameterName")
+public class KalmanFilter<States extends Num, Inputs extends Num, Outputs extends Num> {
+  private final Nat<States> m_states;
+
+  private final LinearSystem<States, Inputs, Outputs> m_plant;
+
+  /** The steady-state Kalman gain matrix. */
+  @SuppressWarnings("MemberName")
+  private Matrix<States, Outputs> m_K;
+
+  /** The state estimate. */
+  @SuppressWarnings("MemberName")
+  private Matrix<States, N1> m_xHat;
+
+  @SuppressWarnings("MemberName")
+  private Matrix<States, States> m_P;
+
+  private Matrix<States, States> m_contQ;
+  private Matrix<Outputs, Outputs> m_contR;
+
+  /**
+   * Constructs a state-space observer with the given plant.
+   *
+   * @param states A Nat representing the states of the system.
+   * @param outputs A Nat representing the outputs of the system.
+   * @param plant The plant used for the prediction step.
+   * @param stateStdDevs Standard deviations of model states.
+   * @param measurementStdDevs Standard deviations of measurements.
+   * @param dtSeconds Nominal discretization timestep.
+   */
+  @SuppressWarnings("LocalVariableName")
+  public KalmanFilter(
+      Nat<States> states,
+      Nat<Outputs> outputs,
+      LinearSystem<States, Inputs, Outputs> plant,
+      Matrix<States, N1> stateStdDevs,
+      Matrix<Outputs, N1> measurementStdDevs,
+      double dtSeconds) {
+    this.m_states = states;
+
+    this.m_plant = plant;
+
+    m_contQ = StateSpaceUtil.makeCovarianceMatrix(states, stateStdDevs);
+    m_contR = StateSpaceUtil.makeCovarianceMatrix(outputs, measurementStdDevs);
+
+    var pair = Discretization.discretizeAQTaylor(plant.getA(), m_contQ, dtSeconds);
+    final var discA = pair.getFirst();
+    final var discQ = pair.getSecond();
+
+    final var discR = Discretization.discretizeR(m_contR, dtSeconds);
+
+    final var C = plant.getC();
+
+    if (!StateSpaceUtil.isDetectable(discA, C)) {
+      var builder =
+          new StringBuilder("The system passed to the Kalman filter is unobservable!\n\nA =\n");
+      builder
+          .append(discA.getStorage().toString())
+          .append("\nC =\n")
+          .append(C.getStorage().toString())
+          .append('\n');
+
+      var msg = builder.toString();
+      MathSharedStore.reportError(msg, Thread.currentThread().getStackTrace());
+      throw new IllegalArgumentException(msg);
+    }
+
+    m_P =
+        new Matrix<>(
+            Drake.discreteAlgebraicRiccatiEquation(
+                discA.transpose(), C.transpose(), discQ, discR));
+
+    // S = CPCᵀ + R
+    var S = C.times(m_P).times(C.transpose()).plus(discR);
+
+    // We want to put K = PCᵀS⁻¹ into Ax = b form so we can solve it more
+    // efficiently.
+    //
+    // K = PCᵀS⁻¹
+    // KS = PCᵀ
+    // (KS)ᵀ = (PCᵀ)ᵀ
+    // SᵀKᵀ = CPᵀ
+    //
+    // The solution of Ax = b can be found via x = A.solve(b).
+    //
+    // Kᵀ = Sᵀ.solve(CPᵀ)
+    // K = (Sᵀ.solve(CPᵀ))ᵀ
+    m_K = S.transpose().solve(C.times(m_P.transpose())).transpose();
+
+    reset();
+  }
+
+  public void reset() {
+    m_xHat = new Matrix<>(m_states, Nat.N1());
+  }
+
+  /**
+   * Returns the steady-state Kalman gain matrix K.
+   *
+   * @return The steady-state Kalman gain matrix K.
+   */
+  public Matrix<States, Outputs> getK() {
+    return m_K;
+  }
+
+  /**
+   * Returns an element of the steady-state Kalman gain matrix K.
+   *
+   * @param row Row of K.
+   * @param col Column of K.
+   * @return the element (i, j) of the steady-state Kalman gain matrix K.
+   */
+  public double getK(int row, int col) {
+    return m_K.get(row, col);
+  }
+
+  /**
+   * Set initial state estimate x-hat.
+   *
+   * @param xhat The state estimate x-hat.
+   */
+  public void setXhat(Matrix<States, N1> xhat) {
+    this.m_xHat = xhat;
+  }
+
+  /**
+   * Set an element of the initial state estimate x-hat.
+   *
+   * @param row Row of x-hat.
+   * @param value Value for element of x-hat.
+   */
+  public void setXhat(int row, double value) {
+    m_xHat.set(row, 0, value);
+  }
+
+  /**
+   * Returns the state estimate x-hat.
+   *
+   * @return The state estimate x-hat.
+   */
+  public Matrix<States, N1> getXhat() {
+    return m_xHat;
+  }
+
+  /**
+   * Returns an element of the state estimate x-hat.
+   *
+   * @param row Row of x-hat.
+   * @return the state estimate x-hat at i.
+   */
+  public double getXhat(int row) {
+    return m_xHat.get(row, 0);
+  }
+
+  /**
+   * Project the model into the future with a new control input u.
+   *
+   * @param u New control input from controller.
+   * @param dtSeconds Timestep for prediction.
+   */
+  @SuppressWarnings("ParameterName")
+  public void predict(Matrix<Inputs, N1> u, double dtSeconds) {
+    this.m_xHat = m_plant.calculateX(m_xHat, u, dtSeconds);
+
+    var pair = Discretization.discretizeAQTaylor(m_plant.getA(), m_contQ, dtSeconds);
+    final var discA = pair.getFirst();
+    final var discQ = pair.getSecond();
+
+    // Pₖ₊₁⁻ = APₖ⁻Aᵀ + Q
+    m_P = discA.times(m_P.times(discA.transpose())).plus(discQ);
+  }
+
+  /**
+   * Correct the state estimate x-hat using the measurements in y.
+   *
+   * @param u Same control input used in the last predict step.
+   * @param y Measurement vector.
+   * @param dtSeconds Time since last correction
+   */
+  @SuppressWarnings("ParameterName")
+  public void correct(Matrix<Inputs, N1> u, Matrix<Outputs, N1> y, double dtSeconds) {
+    final var C = m_plant.getC();
+    final var D = m_plant.getD();
+
+    final var discR = Discretization.discretizeR(m_contR, dtSeconds);
+
+    @SuppressWarnings("LocalVariableName")
+    // S = CPCᵀ + R
+    var S = C.times(m_P).times(C.transpose()).plus(discR);
+
+    // K = (Sᵀ.solve(CPᵀ))ᵀ
+    m_K = S.transpose().solve(C.times(m_P.transpose())).transpose();
+
+    // x̂ₖ₊₁⁺ = x̂ₖ₊₁⁻ + K(y − (Cx̂ₖ₊₁⁻ + Duₖ₊₁))
+    m_xHat = m_xHat.plus(m_K.times(y.minus(C.times(m_xHat).plus(D.times(u)))));
+
+    // Pₖ₊₁⁺ = (I−Kₖ₊₁C)Pₖ₊₁⁻(I−Kₖ₊₁C)ᵀ + Kₖ₊₁RKₖ₊₁ᵀ
+    // Use Joseph form for numerical stability
+    m_P =
+        Matrix.eye(m_states)
+            .minus(m_K.times(C))
+            .times(m_P)
+            .times(Matrix.eye(m_states).minus(m_K.times(C)).transpose())
+            .plus(m_K.times(discR).times(m_K.transpose()));
+  }
+}
